6840d9813d93f26c73b0a7b6c4e99017c23887d1
Mark Lewis <mlewis89@gmail.com>
2024-02-13 14:57:35 +1100

    added userinput validation

--- index.js
+++ index.js
@@ -1,37 +1,69 @@
-const inquirer = require('inquirer');
+const inquirer = require("inquirer");
+var validateColorName = require("validate-color").validateHTMLColorName;
+var validateColorHex = require("validate-color").validateHTMLColorHex;
+
+const isValidText = async (input) => {
+    if (!input) {
+      return "text can't be blank";
+    } else if (input.length > 3) {
+      return "Too many charcters: no more than 3 allowed";
+    } else {
+      return true;
+    }
+  };
+
+  const isValidColour = async (strColor) => {
+    if (!strColor) {
+      return "colour can't be blank";
+    } else if(validateColorName(strColor)) {
+      return true;
+    } else if(validateColorHex(strColor)){
+      return true;
+    }else{
+        return "please enter a valid color";
+    }
+  };
 
 inquirer
-    .prompt([
-        {
-            //text,  
-            type: 'input',
-            name: 'text',
-            message: 'Please enter upto three characters',
-        },
-        {
-            //,  
-            type: 'input',
-            name: 'textColor',
-            message: 'what colour should the text be? (colour keyword or hexadecimal)',
-        },
-        {
-            //,  
-            type: 'checkbox',
-            name: 'shape',
-            message: 'What shap would yopu like the3 backgroud to be',
-            choices: ['circle', 'triangle','square'],
-        },
-        {
-            //,  
-            type: 'input',
-            name: 'shapeColor',
-            message: 'What Colour should the shape be? (colour keyword or hexadecimal)',
-        },
-    ])
-    .then((answers) => {
-        const image = generateSVG(answers);
-    });
+  .prompt([
+    {
+      //text input
+      type: "input",
+      name: "text",
+      message: "Please enter upto three characters",
+      validate: isValidText
+    },
+    {
+      //Text Colour input
+      type: "input",
+      name: "textColor",
+      message:
+        "what colour should the text be? (colour keyword or hexadecimal)",
+      validate: isValidColour
+    },
+    {
+      //Shape selector
+      type: "checkbox",
+      name: "shape",
+      message: "What shap would yopu like the3 backgroud to be",
+      choices: ["circle", "triangle", "square"],
+    },
+    {
+      //Shape Colour Input
+      type: "input",
+      name: "shapeColor",
+      message:
+        "What Colour should the shape be? (colour keyword or hexadecimal)",
+        validate: isValidColour
+    },
+  ])
+  .then((answers) => {
+    const image = generateSVG(answers);
+  });
 
-const generateSVG = (answers)=> {
 
-};
+
+
+
+
+const generateSVG = (answers) => {};
